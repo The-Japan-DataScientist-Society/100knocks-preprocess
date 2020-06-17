e05cbd2ae91c5e1292de9158fc0c31a3a5004a71
noguhiro2002 <noguhiro2015@gmail.com>
2020-06-17 18:05:41 +0000

    100knocks-preprocess ver1.0, For_Azure ver1.0

--- docker/db/init/1_init.sql
+++
@@ -1,5 +0,0 @@
--- create user
-create user padawan password 'padawan12345';
-grant all on database dsdojo_db to padawan;
-grant pg_read_server_files to padawan ;
-grant pg_write_server_files to padawan ;
