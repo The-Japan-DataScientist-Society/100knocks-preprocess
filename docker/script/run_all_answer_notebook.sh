2bb45c8fc9f10c93e8814f06235b9378cdafc9d5
masaya.suzuki <suzukimasaya428@gmail.com>
2021-08-13 22:06:54 +0900

    Fix

--- docker/script/run_all_answer_notebook.sh
+++ docker/script/run_all_answer_notebook.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find ../work/answer -type f -name "*.ipynb" -print0 | xargs -0 -n 1 notebook jupyter nbconvert --to notebook --execute
+find ../work/answer -type f -name "*.ipynb" -print0 | xargs -0 -n 1 notebook jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=1800
