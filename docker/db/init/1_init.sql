2e483e5fe3be91613c3cc303dbe46ab700db1fa5
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-17 08:03:23 +0900

    dsdojo_dbのオーナーを指定する

--- docker/db/init/1_init.sql
+++ docker/db/init/1_init.sql
@@ -3,3 +3,4 @@ create user padawan password 'padawan12345';
 grant all on database dsdojo_db to padawan;
 grant pg_read_server_files to padawan ;
 grant pg_write_server_files to padawan ;
+alter database dsdojo_db owner to padawan;
