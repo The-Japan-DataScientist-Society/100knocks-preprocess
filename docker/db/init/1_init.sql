067222e8928201098a781ab9d90565956cdcec66
Yuto Suzuki <yuto.suzuki.0403@gmail.com>
2024-06-16 22:57:11 +0900

    delete-aux-files

--- docker/db/init/1_init.sql
+++
@@ -1,6 +0,0 @@
--- create user
-CREATE USER padawan PASSWORD 'padawan12345';
-GRANT ALL ON DATABASE dsdojo_db TO padawan;
-GRANT pg_read_server_files TO padawan;
-GRANT pg_write_server_files TO padawan;
-ALTER DATABASE dsdojo_db OWNER TO padawan;
--- install.R
+++
@@ -1,6 +0,0 @@
-install.packages(
-  c("DBI", "RPostgreSQL", "themis"),
-  dependencies = TRUE,
-  error = TRUE,
-  repos = "https://cran.r-project.org"
-)
